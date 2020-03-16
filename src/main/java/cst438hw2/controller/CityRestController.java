b58ee8ff2aed5a23b40ede4321460dbd8c7fd7ec
Dalia <43527456+dollybuzz@users.noreply.github.com>
2020-03-15 23:51:18 -1000

    uploading initial files

---
+++ src/main/java/cst438hw2/controller/CityRestController.java
@@ -0,0 +1,34 @@
+package cst438hw2.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import cst438hw2.domain.*;
+import cst438hw2.service.CityService;
+
+@RestController
+public class CityRestController {
+	
+	@Autowired
+	private CityService cityService;
+	
+	@GetMapping("/api/cities/{city}")
+	public ResponseEntity<CityInfo> getWeather(@PathVariable("city") String cityName) {
+		// TODO your code goes here
+		// TODO delete the following line
+		CityInfo info = cityService.getCityInfo(cityName);
+		
+		if(info == null)
+		{
+			return new ResponseEntity<CityInfo>(HttpStatus.NOT_FOUND);
+		}
+		
+		return new ResponseEntity<CityInfo>(info, HttpStatus.OK);
+	}
+	
+}
