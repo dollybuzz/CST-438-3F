13b4fcaad7b6640cd52518a9e5f5b8fe0814ddc0
Dalia <43527456+dollybuzz@users.noreply.github.com>
2020-03-16 00:59:26 -1000

    adding proper comments from assignment document

--- src/main/java/cst438hw2/WeatherService.java
+++ src/main/java/cst438hw2/WeatherService.java
@@ -12,7 +12,7 @@ import cst438hw2.domain.TempAndTime;
 //Gets time and temperature from the api.openweathermap.org weather server
 
 @Service
-public class WeatherService { //1
+public class WeatherService { //constructor, the value for weatherUrl and apiKey are obtained form the application.properties file
 
 	private static final Logger Log = LoggerFactory.getLogger(WeatherService.class);
 	private RestTemplate restTemplate;
@@ -29,7 +29,9 @@ public class WeatherService { //1
 	public TempAndTime getTempAndTime(String cityName) {
 		ResponseEntity<JsonNode> response = restTemplate.getForEntity(weatherUrl + "?q=" + cityName + "&appid=" +
 		apiKey, JsonNode.class);
-		JsonNode json = response.getBody(); //2
+		JsonNode json = response.getBody(); //takes the text returned by the server and parses it into a tree like data structure called JsonNode
+		//Using the JsonNode object returned by getBody, attributes such as "dt" or "timezone" can be obtained by a get method call
+		//The "temp" attribute is nested inside an attribute named "main". So it first performs do get "main", then secondly performs do get "temp" within "main"
 		Log.info("Status code from weather server:" + response.getStatusCodeValue());
 		
 		double temp = json.get("main").get("temp").asDouble();
